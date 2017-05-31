18af2a3809d11145b61716c38777d41bf723e812
Meelad <mdawood2@sdsu.edu>
2017-05-31 10:05:05 -0700

    fixed space issue for prog1 and did proj2 but has a bug the 3.14 input

--- prog1_2.c
+++ prog1_2.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 
 int main(){
 
     printf("Assignment #1-1, Meelad Dawood, mdawood2@sdsu.edu\n");
     char line[65];
+    printf(">");
     fgets(line, 65, stdin);
     char *word;
     word = strtok(line, " \n\t");
     while (word != NULL) {
-        printf("=%s=\n", word);
+        for(int i = 0; i< strlen(word);i++){
+           if(!isdigit(word[i])){
+	       printf("STR ");
+               break;
+	   } else {
+	       printf("INT ");
+               break;
+	   }
+	}
         word  = strtok(NULL, " \n\t");
     }
  
